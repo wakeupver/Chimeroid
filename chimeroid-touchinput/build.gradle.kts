00066e5ecd34836753f241bb8511e7147ae24d63
wakeupver <verdita1223@gmail.com>
2026-07-26 22:06:46 +0700

    fix(touchinput): switch compiler flag to -Xcontext-parameters

--- chimeroid-touchinput/build.gradle.kts
+++ chimeroid-touchinput/build.gradle.kts
@@ -22,7 +22,7 @@ android {
 kotlin {
     compilerOptions {
         jvmTarget = org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17
-        freeCompilerArgs.add("-Xcontext-receivers")
+        freeCompilerArgs.add("-Xcontext-parameters")
     }
 }
 
