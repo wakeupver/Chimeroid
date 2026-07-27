11b51f562e73b1b50c206b54a94031f06814da7b
wakeupver <verdita1223@gmail.com>
2026-07-27 11:13:43 +0700

    fix(shared): migrate HasSupportFragmentInjector -> HasAndroidInjector

--- chimeroid-shared/src/main/java/com/swordfish/chimeroid/lib/android/RetrogradeActivity.kt
+++ chimeroid-shared/src/main/java/com/swordfish/chimeroid/lib/android/RetrogradeActivity.kt
@@ -20,22 +20,21 @@
 package com.swordfish.chimeroid.lib.android
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 import dagger.android.AndroidInjection
 import dagger.android.AndroidInjector
 import dagger.android.DispatchingAndroidInjector
-import dagger.android.support.HasSupportFragmentInjector
+import dagger.android.HasAndroidInjector
 import javax.inject.Inject
 
-abstract class RetrogradeActivity : FragmentActivity(), HasSupportFragmentInjector {
+abstract class RetrogradeActivity : FragmentActivity(), HasAndroidInjector {
     @Inject
-    lateinit var supportFragmentInjector: DispatchingAndroidInjector<Fragment>
+    lateinit var androidInjector: DispatchingAndroidInjector<Any>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         AndroidInjection.inject(this)
         super.onCreate(savedInstanceState)
     }
 
-    override fun supportFragmentInjector(): AndroidInjector<Fragment>? = supportFragmentInjector
+    override fun androidInjector(): AndroidInjector<Any> = androidInjector
 }
--- chimeroid-shared/src/main/java/com/swordfish/chimeroid/lib/android/RetrogradeAppCompatActivity.kt
+++ chimeroid-shared/src/main/java/com/swordfish/chimeroid/lib/android/RetrogradeAppCompatActivity.kt
@@ -2,21 +2,20 @@ package com.swordfish.chimeroid.lib.android
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.Fragment
 import dagger.android.AndroidInjection
 import dagger.android.AndroidInjector
 import dagger.android.DispatchingAndroidInjector
-import dagger.android.support.HasSupportFragmentInjector
+import dagger.android.HasAndroidInjector
 import javax.inject.Inject
 
-abstract class RetrogradeAppCompatActivity : AppCompatActivity(), HasSupportFragmentInjector {
+abstract class RetrogradeAppCompatActivity : AppCompatActivity(), HasAndroidInjector {
     @Inject
-    lateinit var supportFragmentInjector: DispatchingAndroidInjector<Fragment>
+    lateinit var androidInjector: DispatchingAndroidInjector<Any>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         AndroidInjection.inject(this)
         super.onCreate(savedInstanceState)
     }
 
-    override fun supportFragmentInjector(): AndroidInjector<Fragment>? = supportFragmentInjector
+    override fun androidInjector(): AndroidInjector<Any> = androidInjector
 }
